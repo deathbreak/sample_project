5115f7ad783595b315a4fc0ad988f5875d26112c
zy <1310748243@qq.com>
2019-07-18 11:57:59 +0800

    update file

--- ChangeSettingsName.py
+++ ChangeSettingsName.py
@@ -8,7 +8,7 @@ production_offline_name = 'production_offline.py'
 def ChangeSettingsName(type=1):
     '''
     修改配置文件名称，方便开发。
-    以后的开发中，配置文件将永远引用production，但是为了适配个人开发环境和128测试环境，特写此程序互换配置文件的文件名。
+    配置文件永远引用production，但是为了适配个人开发环境和128测试环境，特写此程序互换配置文件的文件名。
     以后只需要运行一下该程序，便可以在各种开发环境中切换。
 
     :param type:
